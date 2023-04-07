c9712010bfbdbc5b86f3e8c22ab1ed888671605c
Joerg <Joerg.Schmid1995@gmail.com>
2023-04-07 21:41:12 +0200

    Remove props type definition, as this prop is just one function.

--- src/Profile/Post/PostInput.tsx
+++ src/Profile/Post/PostInput.tsx
@@ -1,13 +1,9 @@
 import { postType } from "../Profile";
 import { Button, TextField } from "@mui/material";
 
-type submitPostProps = {
-    submitPost: (post: postType) => void;
-}
-
-export default function PostInput(props: submitPostProps) {
+export default function PostInput({submitPost}: {submitPost: (post: postType) => void}) {
     const handleSubmitButtonClick = (event: any) => {
-        props.submitPost({title: event.target.title.value, message: event.target.message.value});
+        submitPost({title: event.target.title.value, message: event.target.message.value});
         event.preventDefault();
         event.target.title.value = "";
         event.target.message.value = "";
