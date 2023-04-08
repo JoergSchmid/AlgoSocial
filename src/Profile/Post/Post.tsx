f616b596d117ce23880b8981d7fa12267d907d5a
Joerg <Joerg.Schmid1995@gmail.com>
2023-04-08 10:06:55 +0200

    Reformat Posts, so they are multiline and have a min- and max width.

--- src/Profile/Post/Post.tsx
+++ src/Profile/Post/Post.tsx
@@ -4,16 +4,18 @@ import { PostType } from "../Profile";
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#eee',
     ...theme.typography.body2,
-    padding: theme.spacing(1),
+    padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    minWidth: '100px',
+    maxWidth: '400px'
   }));
 
 export default function Post({post}: {post: PostType}) {
     return (
         <Item>
-            <Typography variant="h4">{post.title}</Typography>
-            <Typography variant="body1">{post.message}</Typography>
+            <Typography variant="h4" style={{wordWrap: "break-word"}}>{post.title}</Typography>
+            <Typography variant="body1" style={{wordWrap: "break-word"}}>{post.message}</Typography>
         </Item>
     );
 }
