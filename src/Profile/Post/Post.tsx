b044291d6cda6d248a44195a02289cef85e7c592
Joerg <Joerg.Schmid1995@gmail.com>
2023-04-08 09:36:10 +0200

    Move styled Paper (Post background) to Post.tsx.

--- src/Profile/Post/Post.tsx
+++ src/Profile/Post/Post.tsx
@@ -1,11 +1,19 @@
-import { Typography } from "@mui/material";
+import { Paper, Typography, styled } from "@mui/material";
 import { PostType } from "../Profile";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#eee',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 export default function Post({post}: {post: PostType}) {
     return (
-        <>
+        <Item>
             <Typography variant="h4">{post.title}</Typography>
             <Typography variant="body1">{post.message}</Typography>
-        </>
+        </Item>
     );
 }
--- src/Profile/Post/PostTimeline.tsx
+++ src/Profile/Post/PostTimeline.tsx
@@ -1,23 +1,14 @@
-import { styled } from "@mui/material/styles";
 import { PostType } from "../Profile";
 import Post from "./Post";
 import Grid from "@mui/material/Unstable_Grid2";
-import Paper from "@mui/material/Paper";
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#eee',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
 
 export default function PostTimeline({posts}: {posts: PostType[]}) {
     return <Grid container spacing={4}> {
         posts.map(post => {
             return (
                 <Grid key={post.title}>
-                    <Item><Post post={post}/></Item>
+                    <Post post={post}/>
                 </Grid>
             );
         })
