7cae153ea178178d1c5aaaaba8bff70154cc633d
Joerg <Joerg.Schmid1995@gmail.com>
2023-04-09 13:36:40 +0200

    Install MUI icons. Use a Fab to enable/disable PostInputs.

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -3,6 +3,8 @@ import PostInput from "./Post/PostInput";
 import PostTimeline from "./Post/PostTimeline";
 import { User } from "../App";
 import Grid from "@mui/material/Unstable_Grid2";
+import { Fab } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 export type PostType = {
     title: string,
@@ -12,10 +14,12 @@ export type PostType = {
 
 export default function Profile({user}: {user: User}) {
     const [posts, setPosts] = useState<PostType[]>([]);
+    const [showPostInput, setShowPostInput] = useState<boolean>(false);
     const pic = require("../static/images/profile_pictures/" + user.picture + ".jpg");
 
     function submitPost({title, message}: PostType): void {
         setPosts(posts => [...posts, {title, message, post_id: posts.length}]);
+        setShowPostInput(false);
     }
 
     return (
@@ -29,7 +33,10 @@ export default function Profile({user}: {user: User}) {
                     <h5>{user.description}</h5>
                 </Grid>
             </Grid>
-            <PostInput submitPost={submitPost}/><br/>
+            <Fab aria-label="add" color="primary" style={{margin: 10}} onClick={() => setShowPostInput(!showPostInput)}>
+                <AddIcon />
+            </Fab>
+            {showPostInput && <><PostInput submitPost={submitPost}/><br/></>}
             <PostTimeline posts={posts} />
         </>
     );
