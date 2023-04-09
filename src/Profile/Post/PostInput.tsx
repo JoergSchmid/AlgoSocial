6c45683c6e8d4806b52bea0812c476e71bce5c0b
Joerg <Joerg.Schmid1995@gmail.com>
2023-04-09 12:42:03 +0200

    Implement error messages if TextFields are empty. Values have to be hooked for proper error messages.

--- src/Profile/Post/PostInput.tsx
+++ src/Profile/Post/PostInput.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { PostType } from "../Profile";
 import { Card, Button, TextField } from "@mui/material";
 
 export default function PostInput({submitPost}: {submitPost: (post: PostType) => void}) {
+    const [title, setTitle] = useState<string>(" "); // Contains a space so it doesn´t start with an error message. Space is not really in text field.
+    const [message, setMessage] = useState<string>(" ");
+
     const handleSubmitButtonClick = (event: any) => {
-        submitPost({title: event.target.title.value, message: event.target.message.value});
         event.preventDefault();
+        
+        if (event.target.title.value === "") {
+            setTitle("");
+            return;
+        }
+        if (event.target.message.value === "") {
+            setMessage("");
+            return;
+        }
+        submitPost({title, message});
         event.target.title.value = "";
         event.target.message.value = "";
     }
@@ -12,8 +25,26 @@ export default function PostInput({submitPost}: {submitPost: (post: PostType) =>
     return (
         <Card variant="outlined" style={{width: 345, height: "auto", backgroundColor: "lightgrey"}}>
             <form onSubmit={handleSubmitButtonClick}>
-                <TextField variant="standard" id="title" label="Title" sx={{width: "40ch"}}></TextField><br/>
-                <TextField variant="outlined" id="message" label="Your post" margin="dense" multiline sx={{width: "40ch"}}></TextField><br/>
+                <TextField
+                    variant="standard"
+                    id="title"
+                    label="Title"
+                    onChange={event => setTitle(event.target.value)}
+                    error={title === ""}
+                    helperText={title === "" ? "Please enter a title." : ""}
+                    sx={{width: "40ch"}}
+                ></TextField><br/>
+                <TextField
+                    variant="outlined"
+                    id="message"
+                    label="Your post"
+                    onChange={event => setMessage(event.target.value)}
+                    error={message === ""}
+                    helperText={message === "" ? "Please enter a text." : ""}
+                    margin="dense"
+                    multiline
+                    sx={{width: "40ch"}}
+                ></TextField><br/>
                 <Button variant="contained" type="submit" style={{float: "right"}}>Submit Post</Button>
             </form>
         </Card>
