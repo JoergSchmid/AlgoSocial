ff0d5b6ed94a61d742bf2a87794bfd982c89eb03
Joerg <Joerg.Schmid1995@gmail.com>
2023-04-29 20:15:53 +0200

    Add a delete icon and -function to remove posts.

--- frontend/src/Profile/Post/Post.tsx
+++ frontend/src/Profile/Post/Post.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { Paper, Typography, styled } from "@mui/material";
+import React, { useState } from 'react';
+import { Button, Paper, Typography, styled } from "@mui/material";
+import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
 import { PostType } from "../Profile";
+import { useMutation } from '@apollo/client';
+import { REMOVE_POST } from './gqlRequests';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#eee',
@@ -9,14 +12,41 @@ const Item = styled(Paper)(({ theme }) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
     minWidth: '100px',
-    maxWidth: '400px'
+    maxWidth: '400px',
+    position: "relative"
 }));
 
 export default function Post({ post }: { post: PostType }) {
+    const [showDeleteButton, setShowDeleteButton] = useState<boolean>(false);
+    const [useDeletePost, { error: postDeletionError }] = useMutation(REMOVE_POST);
+
+    function deletePost(): void {
+        useDeletePost({
+            variables: { id: post.id }
+        });
+    }
+
+    // Error log
+    if (postDeletionError) { console.log(postDeletionError) }
+
     return (
-        <Item>
+        <Item onMouseEnter={() => setShowDeleteButton(true)} onMouseLeave={() => setShowDeleteButton(false)} >
             <Typography variant="h4" style={{ wordWrap: "break-word" }}>{post.title}</Typography>
-            <Typography variant="body1" style={{ wordWrap: "break-word" }}>{post.message}</Typography>
+            <Typography variant="body1" style={{ wordWrap: "break-word", marginBottom: "20px" }}>{post.message}</Typography>
+            {showDeleteButton && <Button
+                variant='text'
+                style={{
+                    color: "darkred",
+                    position: "absolute",
+                    right: "5px",
+                    bottom: "5px",
+                    width: "fit-content"
+                }}
+                onClick={deletePost}
+            >
+                <DeleteSweepIcon />
+            </Button>}
+            {postDeletionError && <p className='error'>Error occured when trying to delete post: {postDeletionError.name}</p>}
         </Item>
     );
 }
