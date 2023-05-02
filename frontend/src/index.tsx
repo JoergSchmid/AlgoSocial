3359b4b79fd2f49821c01c0930f85babc7f9a3a6
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-02 20:03:50 +0200

    Remove unneeded import

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: '/graphql',
