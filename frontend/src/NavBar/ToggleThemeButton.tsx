87b9ab5d9a03eefbe748132ac56f353f6cb8b85b
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-03 21:05:55 +0200

    Uppercase for components.

--- frontend/src/NavBar/ToggleThemeButton.tsx
+++ frontend/src/NavBar/ToggleThemeButton.tsx
@@ -2,7 +2,7 @@ import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
 import { Button } from 'react-bootstrap';
 
-export default function toggleThemeButton({ theme, toggleTheme }: { theme: string, toggleTheme: () => void }) {
+export default function ToggleThemeButton({ theme, toggleTheme }: { theme: string, toggleTheme: () => void }) {
     return (
         <Button
             variant='outline-light'
