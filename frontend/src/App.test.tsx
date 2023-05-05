2b2b7e7f6477066181476fe972a35d3d8b2cdce4
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-05 20:45:39 +0200

    Add tests for finding the first example user on screen and adding a post.

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Post from './Profile/Post/Post';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+test('finds the first example user "Joerg"', () => {
+  render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>);
+  const exampleUser = screen.getByText(/Joerg/);
+  expect(exampleUser).toBeInTheDocument();
+});
+
+test('can create a new post', () => {
+  render(
+    <ApolloProvider client={client}>
+      <Post post={{ title: "test_title", message: "test_message", id: -5 }} deletePost={(id: number) => { }}></Post>
+    </ApolloProvider>);
+
+  expect(screen.getByText(/test_title/)).toBeInTheDocument();
+  expect(screen.getByText(/test_message/)).toBeInTheDocument();
 });
