3f5b60ed3ab0e258bd1582a87988d6e38554311d
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-06 09:30:26 +0200

    Create a test_user and a test_post.

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,11 +3,26 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Post from './Profile/Post/Post';
+import Profile, { PostType } from './Profile/Profile';
+import { User } from "./App";
 
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
+
+const test_user: User = {
+  userId: -1,
+  name: "test_user",
+  description: "A user designed for testing",
+  pictureIndex: 0
+}
+
+const test_post: PostType = {
+  id: -1,
+  title: "A test post",
+  message: "This is a test post for testing purposes."
+}
 
 test('finds the first example user "Joerg"', () => {
   render(
@@ -20,9 +35,9 @@ test('finds the first example user "Joerg"', () => {
 
 test('can create a new post', () => {
   render(
-    <Post post={{ title: "test_title", message: "test_message", id: -5 }} deletePost={(id: number) => { }}></Post>
+    <Post post={{ title: test_post.title, message: test_post.message, id: -5 }} deletePost={(id: number) => { }}></Post>
   );
 
-  expect(screen.getByText(/test_title/)).toBeInTheDocument();
-  expect(screen.getByText(/test_message/)).toBeInTheDocument();
+  expect(screen.getByText(RegExp(test_post.title))).toBeInTheDocument();
+  expect(screen.getByText(RegExp(test_post.message))).toBeInTheDocument();
 });
