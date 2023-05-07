3b949ee97b186aa20c84bdf215a755d5d7ff811e
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-07 10:39:12 +0200

    Add new test (WIP) to test adding a post in profile component. Currently checks, if the add/remove icons change when clicked.

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+
 import App from './App';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Post from './Profile/Post/Post';
 import Profile, { PostType } from './Profile/Profile';
 import { User } from "./App";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -41,3 +46,30 @@ test('can generate a complete post', () => {
   expect(screen.getByText(RegExp(test_post.title))).toBeInTheDocument();
   expect(screen.getByText(RegExp(test_post.message))).toBeInTheDocument();
 });
+
+test('can add a Post with PostInput', () => {
+  const wrapper = mount(
+    <ApolloProvider client={client}>
+      <Profile user={test_user} avatar={avatar} changeUser={() => { }} />
+    </ApolloProvider>
+  )
+
+  //PostInput should start closed
+  expect(screen.queryByLabelText(/title/i)).not.toBeInTheDocument();
+  expect(wrapper.find('#removeIcon').exists()).toBe(false);
+  expect(wrapper.find('#addIcon').exists()).toBe(true);
+
+
+  // ToDo: This test fails. wrapper finds 5 elements. No good solution found yet.
+  // Current workarount: Use the .last() node. .first() does NOT work.
+
+  //expect(wrapper.find("#btnTogglePostInput").length).toEqual(1);
+
+  // Click the addIcon <Fab>
+  wrapper.find('#btnTogglePostInput').last().simulate('click');
+  expect(wrapper.find('#removeIcon').exists()).toBe(true);
+  expect(wrapper.find('#addIcon').exists()).toBe(false);
+
+  //ToDo: Enter text and message
+});
+
