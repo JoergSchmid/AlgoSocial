522b0ecd7473d39ee9767df7f3666a8a97674875
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-15 20:52:53 +0200

    Add a selection component and an input Field to Algorithms page.

--- frontend/src/Algorithms/Algorithms.tsx
+++ frontend/src/Algorithms/Algorithms.tsx
@@ -1,9 +1,53 @@
+import { MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
+import { useState } from "react"
 
+
+const ALGORITHMS = [
+    "bubblesort",
+    "quicksort",
+    "isPrime"
+]
 
 export default function Algorithms() {
+    const [algorithm, setAlgorithm] = useState<string>(ALGORITHMS[0]);
+    const [input, setInput] = useState<number | null>(null)
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setAlgorithm(event.target.value);
+    }
+
     return (
         <>
-            <h3>Algorithms Page</h3>
+            <h3>Algorithms Page - WIP</h3>
+            <Select
+                id="algorithmSelection"
+                data-testid="algorithmSelection"
+                autoWidth
+                value={algorithm}
+                onChange={handleChange}
+            >
+                {ALGORITHMS.map((alg) => (
+                    <MenuItem
+                        key={alg}
+                        value={alg}
+                    >
+                        {alg}
+                    </MenuItem>
+                ))}
+            </Select>
+            <br />
+            <TextField
+                variant="outlined"
+                type="number"
+                id="input_isPrime"
+                data-testid="input_isPrime"
+                label="input number for prime test"
+                onChange={event => setInput(+event.target.value)}
+                helperText={input === null ? "Please enter a number." : ""}
+                margin="dense"
+                multiline
+                sx={{ width: "40ch" }}
+            />
         </>
     )
 }
