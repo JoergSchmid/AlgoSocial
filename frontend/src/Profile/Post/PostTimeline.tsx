b1bbcf30ea9d4fa253d4a2c5fd96d0a2f7c4edbc
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-26 22:16:19 +0200

    Add <AlgorithmPost> component as alternative to <Post>, when the post contains a task. Shows original input and result, more to be added.

--- frontend/src/Profile/Post/PostTimeline.tsx
+++ frontend/src/Profile/Post/PostTimeline.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PostType } from "../Profile";
 import Post from "./Post";
 import Grid from "@mui/material/Unstable_Grid2";
+import AlgorithmPost from './AlgorithmPost';
 
 
 export default function PostTimeline({ posts, deletePost }: { posts: PostType[], deletePost: (id: number) => void }) {
@@ -9,7 +10,9 @@ export default function PostTimeline({ posts, deletePost }: { posts: PostType[],
         posts.map(post => {
             return (
                 <Grid key={post.title}>
-                    <Post post={post} deletePost={deletePost} />
+                    {post.taskId && post.taskId > 0 ?
+                        <AlgorithmPost post={post} deletePost={deletePost} /> :
+                        <Post post={post} deletePost={deletePost} />}
                 </Grid>
             );
         })
