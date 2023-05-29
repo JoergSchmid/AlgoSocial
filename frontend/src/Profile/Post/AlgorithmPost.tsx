24b0dd17a12e3e7220ea70005077f9f549ce5020
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-29 20:44:20 +0200

    Extract Card style definition and delete button from both Post components and write them in new PostCard component.

--- frontend/src/Profile/Post/AlgorithmPost.tsx
+++ frontend/src/Profile/Post/AlgorithmPost.tsx
@@ -1,26 +1,13 @@
-import React, { useState } from 'react';
-import { Button, Paper, Typography, styled } from "@mui/material";
-import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
+import React from 'react';
+import { Typography } from "@mui/material";
 import { AlgorithmType, PostType, Status } from "../Profile";
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#eee',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    minWidth: '100px',
-    maxWidth: '400px',
-    borderRadius: "16px",
-    position: "relative"
-}));
+import PostCard from './PostCard';
 
 export default function AlgorithmPost({ availableAlgorithms, post, deletePost }: {
     availableAlgorithms: AlgorithmType[],
     post: PostType,
     deletePost: (id: number) => void,
 }) {
-    const [showDeleteButton, setShowDeleteButton] = useState<boolean>(false);
 
     const getAlgorithmDisplayName = (name: string): string => {
         const algorithm = availableAlgorithms.find(alg => alg.name === name);
@@ -33,21 +20,21 @@ export default function AlgorithmPost({ availableAlgorithms, post, deletePost }:
     }
 
     const getStatusBackgroundColor = (): string => {
-        let postStatus = post.task?.status.toString();
-        return postStatus ===
-            Status[Status.ERROR] ? "rgba(255,0,0,0.7)" : postStatus ===
-                Status[Status.CALCULATING] ? "rgba(255,255,0,0.5)" :
-            "rgba(0,255,0,0.2)"
+        if (!post.task) { return ""; }
+        const postStatus = post.task.status;
+
+        const STATUS_COLOR_MAPPING = {
+            [Status.ERROR]: "rgba(255,0,0,0.7)",
+            [Status.CALCULATING]: "rgba(255,255,0,0.5)",
+            [Status.DONE]: "rgba(0,255,0,0.2)"
+        }
+
+        return STATUS_COLOR_MAPPING[postStatus];
     }
 
     return (
         <>
-            {post.task && <Item
-                style={{ backgroundColor: getStatusBackgroundColor() }}
-                data-testid="postContainer"
-                onMouseEnter={() => { if (post.id > 0) setShowDeleteButton(true) }}
-                onMouseLeave={() => setShowDeleteButton(false)}
-            >
+            {post.task && <PostCard post={post} deletePost={deletePost} backgroundColor={getStatusBackgroundColor()}>
                 <Typography variant="h4" style={{ wordWrap: "break-word" }}>{post.title}</Typography>
                 <Typography
                     variant="h6"
@@ -61,21 +48,7 @@ export default function AlgorithmPost({ availableAlgorithms, post, deletePost }:
                     variant="body1"
                     style={{ wordWrap: "break-word", marginBottom: "20px" }}
                 >{removeBrackets(post.task.result)}</Typography>
-                {showDeleteButton && <Button
-                    variant='text'
-                    data-testid="btn_delete"
-                    style={{
-                        color: "darkred",
-                        position: "absolute",
-                        right: "5px",
-                        bottom: "5px",
-                        width: "fit-content"
-                    }}
-                    onClick={() => deletePost(post.id)}
-                >
-                    <DeleteSweepIcon />
-                </Button>}
-            </Item>}
+            </PostCard>}
         </>
     );
 }
--- frontend/src/Profile/Post/Post.tsx
+++ frontend/src/Profile/Post/Post.tsx
@@ -1,47 +1,17 @@
-import React, { useState } from 'react';
-import { Button, Paper, Typography, styled } from "@mui/material";
-import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
+import React from 'react';
+import { Typography } from "@mui/material";
 import { PostType } from "../Profile";
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#eee',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    minWidth: '100px',
-    maxWidth: '400px',
-    borderRadius: "16px",
-    position: "relative"
-}));
+import PostCard from './PostCard';
 
 export default function Post({ post, deletePost }: { post: PostType, deletePost: (id: number) => void }) {
-    const [showDeleteButton, setShowDeleteButton] = useState<boolean>(false);
-
     return (
-        <Item
-            data-testid="postContainer"
-            onMouseEnter={() => { if (post.id > 0) setShowDeleteButton(true) }}
-            onMouseLeave={() => setShowDeleteButton(false)}
-        >
+        <PostCard post={post} deletePost={deletePost}>
+
             <Typography variant="h4" style={{ wordWrap: "break-word" }}>{post.title}</Typography>
             <Typography
                 variant="body1"
                 style={{ wordWrap: "break-word", marginBottom: "20px" }}>{post.message}</Typography>
-            {showDeleteButton && <Button
-                variant='text'
-                data-testid="btn_delete"
-                style={{
-                    color: "darkred",
-                    position: "absolute",
-                    right: "5px",
-                    bottom: "5px",
-                    width: "fit-content"
-                }}
-                onClick={() => deletePost(post.id)}
-            >
-                <DeleteSweepIcon />
-            </Button>}
-        </Item>
+
+        </PostCard>
     );
 }
