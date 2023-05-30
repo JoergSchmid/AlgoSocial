ae6f600e184ae20229f6269d6ef3ca652be93232
Joerg <Joerg.Schmid1995@gmail.com>
2023-05-30 20:43:58 +0200

    Add unit test for isPrime Algorithm.

---
+++ backend/src/test/java/de/algoSocial/backend/algorithms/IsPrimeTest.java
@@ -0,0 +1,22 @@
+package de.algoSocial.backend.algorithms;
+
+import de.algosocial.backend.algorithms.AlgorithmCalculations;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IsPrimeTest {
+
+    @Test
+    void testIsPrime_standard() {
+        int[] inputPrime = {2, 3, 5, 7, 11, 13, 17, 19};
+        int[] inputNotPrime = {-5, 0, 1, 4, 6, 9, 10, 15};
+
+        for (int i : inputPrime)
+            Assertions.assertTrue(AlgorithmCalculations.isPrime(i));
+
+        for (int i : inputNotPrime)
+            Assertions.assertFalse(AlgorithmCalculations.isPrime(i));
+    }
+}
