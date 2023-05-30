340c50d45319fb4f083b3312fb9a423fb60343e9
FelixFifi <FelixFifi@users.noreply.github.com>
2023-05-31 00:06:34 +0200

    The issue was a combination of multiple issues: - The package of the test class was different than that of the main application, which resulted in the "configuration not found" error - This "configuration not found" error prevented us from using the @SpringBootTest annotation instead - Not using @SpringBootTest resulted in all our @Controller and @Autowire JPA Repositories not being automagically loaded by spring, as the application was not initialized fully

---
+++ backend/src/test/java/de/algoSocial/backend/PostTest.java
@@ -0,0 +1,46 @@
+package de.algoSocial.backend;
+
+import de.algosocial.backend.PostController;
+import de.algosocial.backend.PostRepository;
+import de.algosocial.backend.TaskService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.AutoConfigureGraphQl;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.graphql.test.tester.GraphQlTester;
+
+@SpringBootTest
+@AutoConfigureGraphQl
+@AutoConfigureGraphQlTester
+public class PostTest {
+    @Autowired
+    private GraphQlTester graphQlTester;
+
+    @Autowired
+    private PostController postController;
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private TaskService taskService;
+
+    @Test
+    void postControllerGetsLoaded() {
+        // When first starting to write a test, first check that the correct application context is loaded and some sample @AutoWired fields could be populated
+        // This checks that the SpringBootApplication is initialized normally with all components we need.
+        // This catches error like the test package being named differently, which was the case here, or only using @GraphQlTest instead of @SpringBootTest, which does not perform all spring magic like initializing JPA repositories
+        Assertions.assertNotNull(postController);
+        Assertions.assertNotNull(postRepository);
+        Assertions.assertNotNull(taskService);
+    }
+
+    @Test
+    void getAllTest() {
+        this.graphQlTester.documentName("allPosts")
+                .execute()
+                .path("allPosts[*].title")
+                .entityList(String.class)
+                .hasSizeGreaterThan(0);
+    }
+}
