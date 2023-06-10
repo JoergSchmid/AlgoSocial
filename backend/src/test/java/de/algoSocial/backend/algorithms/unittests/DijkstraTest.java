893ee7318a3393fe15d07b0bea387e135a929f1c
Joerg <Joerg.Schmid1995@gmail.com>
2023-06-10 13:45:52 +0200

    Add a test, testing for a more complex graph. Currently failing.

--- backend/src/test/java/de/algoSocial/backend/algorithms/unittests/DijkstraTest.java
+++ backend/src/test/java/de/algoSocial/backend/algorithms/unittests/DijkstraTest.java
@@ -49,10 +49,67 @@ public class DijkstraTest {
         // Test path
         Assertions.assertEquals(d.getPath(), "a,c,d");
     }
+
+    @Test
+    void createComplexTree() {
+        List<DijkstraNode> nodes = new ArrayList<DijkstraNode>();
+
+        DijkstraNode a = new DijkstraNode("a");
+        DijkstraNode b = new DijkstraNode("b");
+        DijkstraNode c = new DijkstraNode("c");
+        DijkstraNode d = new DijkstraNode("d");
+        DijkstraNode e = new DijkstraNode("e");
+        DijkstraNode f = new DijkstraNode("f");
+        DijkstraNode g = new DijkstraNode("g");
+        DijkstraNode h = new DijkstraNode("h");
+        DijkstraNode i = new DijkstraNode("i");
+        DijkstraNode j = new DijkstraNode("j");
+
+        a.connectWith(b, 5);
+        a.connectWith(c, 3);
+        a.connectWith(f, 9);
+        a.connectWith(h, 1);
+        a.connectWith(i, 6);
+
+        b.connectWith(d, 13);
+        b.connectWith(i, 3);
+        b.connectWith(j, 19);
+
+        c.connectWith(f, 2);
+        c.connectWith(h, 3);
+
+        d.connectWith(f, 7);
+        d.connectWith(g, 1);
+        d.connectWith(h, 5);
+        d.connectWith(j, 6);
+
+        e.connectWith(g, 11);
+        e.connectWith(i, 8);
+
+        g.connectWith(j, 4);
+
+        // Move nodes into list
+        nodes.add(a);
+        nodes.add(b);
+        nodes.add(c);
+        nodes.add(d);
+        nodes.add(e);
+        nodes.add(f);
+        nodes.add(g);
+        nodes.add(h);
+        nodes.add(i);
+        nodes.add(j);
+
         // Establish tree
         Dijkstra dijkstra = new Dijkstra(nodes);
 
-        // Shortest path from a to d (first to last element in list) is a-c-d = 6
-        Assertions.assertEquals(dijkstra.start(), 6);
+        // Test distances
+        // Shortest path a to j is a -3- c -2- f -7- d -1- g -4- j = 17
+        Assertions.assertEquals(17, j.getDistanceToInitialNode());
+        Assertions.assertEquals(13, g.getDistanceToInitialNode()); // w/o last node
+
+        // Test paths
+        Assertions.assertEquals(j.getPath(), "a,c,f,d,g,j");
+        Assertions.assertEquals(g.getPath(), "a,c,f,d,g");
     }
 }
