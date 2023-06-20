b8d6f68b43b80de871a40814fac0836bb3ac8e57
Joerg <Joerg.Schmid1995@gmail.com>
2023-06-20 20:41:57 +0200

    Change BST Test and add some, to test the coming change in displaying the Tree in a tree structure.

--- backend/src/test/java/de/algoSocial/backend/algorithms/unittests/BinarySearchTreeTest.java
+++ backend/src/test/java/de/algoSocial/backend/algorithms/unittests/BinarySearchTreeTest.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class BinarySearchTreeTest {
 
     @Test
-    void createSearchTree() {
-        List<Integer> input = Arrays.asList(6,5,2,9,1,4,0,8,3,7);
-        List<Integer> expected = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+    void createSimpleSearchTree() {
+        List<Integer> input = Arrays.asList(2,1,3);
+        String expected = "((,1,),2,(,3,))";
 
         BinarySearchTree binarySearchTree = new BinarySearchTree();
 
@@ -23,16 +23,46 @@ public class BinarySearchTreeTest {
     }
 
     @Test
+    void createPresortedSearchTree() {
+        List<Integer> input = Arrays.asList(4,3,2,1);
+        String expected = "((((,1,),2,),3,),4,)";
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        for (int i : input)
+            binarySearchTree.insert(i);
+
+        String result = binarySearchTree.getTree();
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void createBiggerSearchTree() {
+        List<Integer> input = Arrays.asList(6,5,2,9,1,4,0,8,3,7);
+        String expected = "(((((,0,),1,),2,((,3,),4,)),5,),6,(((,7,),8,),9,))";
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        for (int i : input)
+            binarySearchTree.insert(i);
+
+        String result = binarySearchTree.getTree();
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
     void noDuplicateEntriesInTree() {
         // If the same number gets inserted multiple times, we expect only one number remaining in the tree.
-        List<Integer> input = Arrays.asList(2,4,3,3,1,5,3);
-        List<Integer> expected = Arrays.asList(1,2,3,4,5);
+        List<Integer> input = Arrays.asList(2,1,3,1);
+        String expected = "((,1,),2,(,3,))";
 
         BinarySearchTree binarySearchTree = new BinarySearchTree();
         for (int i : input)
             binarySearchTree.insert(i);
 
-        List<Integer> result = binarySearchTree.getAll();
+        String result = binarySearchTree.getTree();
 
         Assertions.assertEquals(expected, result);
     }
