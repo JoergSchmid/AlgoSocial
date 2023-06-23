968c18c3730821ea2226654f8e9f58a1c378e0c9
Joerg <Joerg.Schmid1995@gmail.com>
2023-06-23 18:42:13 +0200

    TaskService class now uses Algorithm class object.

--- backend/src/main/java/de/algosocial/backend/TaskService.java
+++ backend/src/main/java/de/algosocial/backend/TaskService.java
@@ -1,6 +1,7 @@
 package de.algosocial.backend;
 
 import de.algosocial.backend.algorithms.AlgorithmController;
+import de.algosocial.backend.algorithms.AlgorithmFactory;
 import de.algosocial.backend.algorithms.AlgorithmSuperClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -18,43 +19,30 @@ public class TaskService {
 
     @Async
     public void startTask(Task task) throws InterruptedException {
-        List<String> input = task.getInput();
+        AlgorithmSuperClass algorithm = AlgorithmFactory.getClass(task.getAlgorithm());
+
+        // ToDo: Add catching wrong task status
+
+        if (algorithm == null) {
+            task.setError("Error: Requested algorithm not found.");
+            taskRepository.save(task);
+            errorLogRepository.save(new ErrorLog(task.getUserId(), task.getId(), task.getInput(), task.getError()));
+            return;
+        }
+
         String result;
 
         try {
-            if (Objects.equals(task.getAlgorithm(), "bubblesort")) {
-                result = AlgorithmController.bubbleSort(stringToIntList(input.get(0))).toString();
-            } else if (Objects.equals(task.getAlgorithm(), "quicksort")) {
-                result = AlgorithmController.quickSort(stringToIntList(input.get(0))).toString();
-            } else if (Objects.equals(task.getAlgorithm(), "isprime")) {
-                result = AlgorithmController.isPrime(Integer.parseInt(input.get(0))) ? "prime" : "not prime";
-            } else if (Objects.equals(task.getAlgorithm(), "binarySearchTree")) {
-                result = AlgorithmController.binarySearchTree(stringToIntList(input.get(0)));
-            } else if (Objects.equals(task.getAlgorithm(), "binarySearchTreeFindNumber")) {
-                result = String.valueOf(AlgorithmController.binarySearchTreeFindNumber(stringToIntList(input.get(0)), Integer.parseInt(input.get(1))));
-            } else if (Objects.equals(task.getAlgorithm(), "dijkstra")) {
-                result = AlgorithmController.dijkstra(input.get(0), input.get(1));
-            } else {
-                task.setError("Error: Requested algorithm not found.");
-                taskRepository.save(task);
-                return;
-            }
-            task.setStatus(Task.Status.DONE);
-            task.setResult(result);
-            taskRepository.save(task);
+            result = algorithm.getResult(task.getInput());
         } catch (Exception e) {
-            task.setError("Error: Invalid input");
+            task.setError("Error: Invalid input.");
             taskRepository.save(task);
             errorLogRepository.save(new ErrorLog(task.getUserId(), task.getId(), task.getInput(), task.getError()));
+            return;
         }
 
-    }
-
-    private List<Integer> stringToIntList(String string) {
-        List<Integer> list = new ArrayList<>();
-        String[] strings = string.split(",");
-        for (String str : strings)
-            list.add(Integer.parseInt(str));
-        return list;
+        task.setStatus(Task.Status.DONE);
+        task.setResult(result);
+        taskRepository.save(task);
     }
 }
