f8f3d1ac06b62a4a3a453a34f18cf24e93ed5b55
Joerg <Joerg.Schmid1995@gmail.com>
2023-06-24 10:38:54 +0200

    Delete Algorithm(Repository) Classes, since we no longer need to store algorithms in a db table.

--- backend/src/main/java/de/algosocial/backend/algorithms/AlgorithmRepository.java
+++
@@ -1,8 +0,0 @@
-package de.algosocial.backend.algorithms;
-
-import de.algosocial.backend.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlgorithmRepository extends CrudRepository<Algorithm, Integer> {
-    Task findById(int id);
-}
