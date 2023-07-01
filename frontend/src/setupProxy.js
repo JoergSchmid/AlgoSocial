455f5bc8e74123b184fb6ebb834f972e3beab5c4
FelixFifi <FelixFifi@users.noreply.github.com>
2023-07-01 22:18:36 +0200

    tmp

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,10 @@
+const backend_server = "backend:8080"; // process.env.BACKEND_SERVER;
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const { env } = require('process');
+
 const proxy = {
-    target: 'http://localhost:8080',
+    target: 'http://' + backend_server,
     changeOrigin: true
 }
 module.exports = function (app) {
