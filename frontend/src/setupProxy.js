d7a061f2424a27a9657824f8c8abd6fdc0ecd401
Joerg <Joerg.Schmid1995@gmail.com>
2023-07-02 11:37:58 +0200

    Change proxy url on frontend.

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,9 @@
+const BACKEND_SERVER = "backend:8080";
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const proxy = {
-    target: 'http://localhost:8080',
+    target: 'http://' + BACKEND_SERVER,
     changeOrigin: true
 }
 module.exports = function (app) {
