5933c50fad6dc5a7a749807fa37d1948cedc851d
Joerg <Joerg.Schmid1995@gmail.com>
2023-07-10 19:25:47 +0200

    Use environment variable defined in Dockerfile to inject the url for our backend.

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,9 +1,10 @@
-const BACKEND_SERVER = "backend:8080";
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// Get url for backend from env defined in Dockerfile (or alternatively in .env file)
+const backend_url = process.env.REACT_APP_BACKEND_URL == null ? "localhost:8080" : process.env.REACT_APP_BACKEND_URL;
+
 const proxy = {
-    target: 'http://' + BACKEND_SERVER,
+    target: 'http://' + backend_url,
     changeOrigin: true
 }
 module.exports = function (app) {
